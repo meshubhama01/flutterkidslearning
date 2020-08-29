97326b9cda0276674d08eaa689e867ce349fb0e6
Shubham kumar <meshubhama01@gmail.com>
2020-08-29 12:08:09 +0530

    main code

---
+++ Reference App/lib/drawing_painter.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'constants.dart';
+
+class DrawingPainter extends CustomPainter {
+  DrawingPainter({this.offsetPoints});
+  List<Offset> offsetPoints;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    for (int i = 0; i < offsetPoints.length - 1; i++) {
+      if (offsetPoints[i] != null && offsetPoints[i + 1] != null) {
+        canvas.drawLine(offsetPoints[i], offsetPoints[i + 1], kDrawingPaint);
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(DrawingPainter oldDelegate) => true;
+}
