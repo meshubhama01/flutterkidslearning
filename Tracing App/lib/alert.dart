9d6ad9f039fc79e6d2c4f8588a607363efd85454
Shubham kumar <meshubhama01@gmail.com>
2020-09-08 23:18:52 +0530

    Update alert.dart

--- Tracing App/lib/alert.dart
+++ Tracing App/lib/alert.dart
@@ -1,46 +1,39 @@
-// import 'package:flutter/material.dart';
-// import 'package:rflutter_alert/rflutter_alert.dart';
-// // import 'main.dart';
+import 'package:flutter/material.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+import 'package:kid_learn/main.dart';
 
-// testPopup(BuildContext dialogContext, int numberActual) {
-//   var alertStyle = AlertStyle(
-//     animationType: AnimationType.grow,
-//     overlayColor: Colors.black87,
-//     isCloseButton: true,
-//     isOverlayTapDismiss: true,
-//     titleStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-//     descStyle: TextStyle(fontWeight: FontWeight.w500, fontSize: 16),
-//     animationDuration: Duration(milliseconds: 400),
-//   );
+testPopup(BuildContext dialogContext) {
+  var alertStyle = AlertStyle(
+    animationType: AnimationType.grow,
+    overlayColor: Colors.black87,
+    isCloseButton: true,
+    isOverlayTapDismiss: true,
+    titleStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+    descStyle: TextStyle(fontWeight: FontWeight.w500, fontSize: 16),
+    animationDuration: Duration(milliseconds: 400),
+  );
 
-//   Alert(
-//       context: dialogContext,
-//       style: alertStyle,
-//       title: "Report",
-//       desc: "yha pr accuracy btana h",
-//       content: Column(
-//         mainAxisSize: MainAxisSize.min,
-//         children: <Widget>[
-
-//           Text(" Number Given: $numberActual")
-//         ],
-//       ),
-//       buttons: [
-//         DialogButton(
-//           child: Text(
-//             "Retry",
-//             style: TextStyle(color: Colors.white, fontSize: 18),
-//           ),
-//           onPressed: () {},
-//           color: Colors.black54,
-//         ),
-//         DialogButton(
-//           child: Text(
-//             "Next",
-//             style: TextStyle(color: Colors.white, fontSize: 18),
-//           ),
-//           onPressed: () {},
-//           color: Colors.black54,
-//         )
-//       ]).show();
-// }
+  Alert(
+      context: dialogContext,
+      style: alertStyle,
+      title: "Report",
+      desc: "yha pr accuracy btana h",
+      buttons: [
+        DialogButton(
+          child: Text(
+            "Retry",
+            style: TextStyle(color: Colors.white, fontSize: 18),
+          ),
+          onPressed: () {},
+          color: Colors.black54,
+        ),
+        DialogButton(
+          child: Text(
+            "Next",
+            style: TextStyle(color: Colors.white, fontSize: 18),
+          ),
+          onPressed: () {},
+          color: Colors.black54,
+        )
+      ]).show();
+}
