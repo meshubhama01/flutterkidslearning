d72759b3e9ded71aff8317650811639a42dd96b3
Shubham kumar <meshubhama01@gmail.com>
2020-09-08 23:18:05 +0530

    Update constants.dart

--- Tracing App/lib/constants.dart
+++ Tracing App/lib/constants.dart
@@ -1,24 +1,9 @@
-import 'package:flutter/material.dart';
+class Constants {
+  static double canvasSize = 300;
+  static double borderSize = 2;
 
-const double kCanvasSize = 400.0;
-const double kStrokeWidth = 12.0;
-const Color kBlackBrushColor = Colors.black;
-const bool kIsAntiAlias = true;
-const int kModelInputSize = 28;
-const double kCanvasInnerOffset = 80.0;
-const Color kBrushBlack = Colors.black;
-const Color kBrushWhite = Colors.white;
+  static double imageSize = 300;
+  static int mnistImageSize = 28;
 
-final Paint kDrawingPaint = Paint()
-  ..strokeCap = StrokeCap.square
-  ..isAntiAlias = kIsAntiAlias
-  ..color = kBrushBlack
-  ..strokeWidth = kStrokeWidth;
-
-final Paint kWhitePaint = Paint()
-  ..strokeCap = StrokeCap.square
-  ..isAntiAlias = kIsAntiAlias
-  ..color = kBrushWhite
-  ..strokeWidth = kStrokeWidth;
-
-final kBackgroundPaint = Paint()..color = kBrushBlack;
+  static double strokeWidth = 8;
+}
