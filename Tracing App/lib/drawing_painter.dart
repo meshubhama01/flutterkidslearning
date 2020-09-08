ea4dd84631a6ba63c6209335a053251b781bbbd7
Shubham kumar <meshubhama01@gmail.com>
2020-09-08 23:17:02 +0530

    Update drawing_painter.dart

--- Tracing App/lib/drawing_painter.dart
+++ Tracing App/lib/drawing_painter.dart
@@ -1,25 +1,27 @@
 import 'package:flutter/material.dart';
 import 'constants.dart';
 
-final Paint kdrawingPaint = Paint()
-  ..strokeCap = StrokeCap.square
-  ..isAntiAlias = kIsAntiAlias
-  ..color = kBlackBrushColor
-  ..strokeWidth = kStrokeWidth;
-
 class DrawingPainter extends CustomPainter {
-  DrawingPainter({this.offsetPoints});
-  List<Offset> offsetPoints;
+  final List<Offset> points;
+
+  DrawingPainter(this.points);
+
+  final Paint _paint = Paint()
+    ..strokeCap = StrokeCap.round
+    ..color = Colors.black
+    ..strokeWidth = Constants.strokeWidth;
 
   @override
   void paint(Canvas canvas, Size size) {
-    for (int i = 0; i < offsetPoints.length - 1; i++) {
-      if (offsetPoints[i] != null && offsetPoints[i + 1] != null) {
-        canvas.drawLine(offsetPoints[i], offsetPoints[i + 1], kdrawingPaint);
+    for (int i = 0; i < points.length - 1; i++) {
+      if (points[i] != null && points[i + 1] != null) {
+        canvas.drawLine(points[i], points[i + 1], _paint);
       }
     }
   }
 
   @override
-  bool shouldRepaint(DrawingPainter oldDelegate) => true;
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
 }
